a408e8d43713f685c1101c95225481811e4c51da
anshitaagrawal09 <anshitaagrawal75@gmail.com>
2025-01-20 11:43:20 +0530

    Practice Problem

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+public class EvenOdd{
+public static void main(String[] args){
+System.out.println("Even NUmbers");
+for(int i=1;i<=10;i++){
+if(i%2==0){
+System.out.println(i);
+}
+}
+System.out.println("Odd Numbers");
+for(int i=1;i<=10;i++){
+if(i%2!=0){
+System.out.println(i);
+}
+}
+}
+}
